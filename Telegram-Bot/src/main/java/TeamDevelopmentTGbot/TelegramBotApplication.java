bab0f1f27cb464b6302fe840b498a1c3d7be0dc1
1 <glotov-aleks@mail.ru>
2023-12-26 18:30:20 +0400

    рабочий код для приветствия

--- Telegram-Bot/src/main/java/TeamDevelopmentTGbot/TelegramBotApplication.java
+++ Telegram-Bot/src/main/java/TeamDevelopmentTGbot/TelegramBotApplication.java
@@ -2,13 +2,14 @@ package TeamDevelopmentTGbot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class TelegramBotApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TelegramBotApplication.class, args);
 	}
-
 
 }
