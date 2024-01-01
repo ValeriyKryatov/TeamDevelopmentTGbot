a221015dd39518d0233e360b81ce09678ee4b8fb
1 <glotov-aleks@mail.ru>
2024-01-01 17:27:22 +0400

    рабочая версия для моей идеи

--- Telegram-Bot/src/main/java/TeamDevelopmentTGbot/listener/TelegramBotUpdateListener.java
+++ Telegram-Bot/src/main/java/TeamDevelopmentTGbot/listener/TelegramBotUpdateListener.java
@@ -4,7 +4,9 @@ import TeamDevelopmentTGbot.model.NotificationTask;
 import TeamDevelopmentTGbot.repository.NotificationRepository;
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
+import com.pengrad.telegrambot.model.CallbackQuery;
 import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.model.request.*;
 import com.pengrad.telegrambot.request.SendMessage;
 import jakarta.annotation.PostConstruct;
 import org.slf4j.Logger;
@@ -30,44 +32,177 @@ public class TelegramBotUpdateListener implements UpdatesListener {
 
     private final TelegramBot bot;
     private final NotificationRepository repository;
+    private HermitageInlineKeyboardAb hermitageInlineKeyboardAb;
 
-    public TelegramBotUpdateListener(TelegramBot bot, NotificationRepository repository) {
+
+    public TelegramBotUpdateListener(TelegramBot bot,
+                                     NotificationRepository repository,
+                                     HermitageInlineKeyboardAb hermitageInlineKeyboardAb) {
         this.bot = bot;
         this.repository = repository;
+        this.hermitageInlineKeyboardAb = hermitageInlineKeyboardAb;
     }
+
+//    public TelegramBotUpdateListener(TelegramBot bot) {
+//        this.bot = bot;
+//    }
 
     @PostConstruct
     public void init() {
         bot.setUpdatesListener(this);
     }
-    private final Map<Long, Boolean> userAlreadyInteracted = new HashMap<>(); // Карта для отслеживания взаимодействия с пользователями
 
     @Override
     public int process(List<Update> updates) {
+        String text = "";
+        long  chatId = 0;
         for (Update update : updates) {
-            var text = update.message().text();
-            var chatId = update.message().chat().id();
+            try {
+                text = update.message().text();
+                chatId = update.message().chat().id();
+                System.out.println(update.toString());
 
-            if (userAlreadyInteracted.containsKey(chatId)) {
-                // Пользователь уже обращался
-                if (text.equalsIgnoreCase("/start")) {
-                    // Если пользователь выбирает меню
-                    sendMessage(chatId, "Выберите опцию из меню...");
-                } else {
-                    // Обрабатываем запросы пользователя
-                    sendMessage(chatId, "Вы выбрали: " + text);
+            } catch (NullPointerException e){
+                e.getStackTrace();
+                logger.error("Cannot NullPointerException: {}");
+            }
+            System.out.println("  Этот текст, пришел от бота ==> " + text);
+
+            if ("/start".equals(text)) {
+                sendMessage(chatId, "Добро пожаловать в бот!");
+
+            } else if ("/menu".equals(text)){
+                SendMessage response = new SendMessage(chatId, "Меню")
+                        .replyMarkup(new ReplyKeyboardMarkup(new String[][] {
+                                {"Приют", "Старт"},
+                                {"Поддержка"}
+                        }).resizeKeyboard(true));
+                bot.execute(response);
+//                continue;
+            } else  if ( "/shelter".equals(text) ){
+                SendMessage response = new SendMessage(chatId, "Меню1")
+                        .replyMarkup(new ReplyKeyboardMarkup(new String[][] {
+                                {"Приют1"},
+                                {"Поддержка1"}
+                        }).resizeKeyboard(true));
+                bot.execute(response);
+//**************************************************************************************
+                Keyboard keyboard = new ReplyKeyboardMarkup(
+                        new KeyboardButton[]{
+                                new KeyboardButton("text Обо мне "),
+                                new KeyboardButton("contact").requestContact(true),
+                                new KeyboardButton("location").requestLocation(true)
+                        }
+                );
+                SendMessage response1 = new SendMessage(chatId, "Меню2")
+                        .replyMarkup(keyboard);
+
+                bot.execute(response1);
+                bot.execute(new SendMessage(chatId, "Вы находитесь в разделе Приют "));
+                continue;
+//****************************************************************************************
+            } else  if ("/volunteers".equals(text)){
+
+//                SendMessage response = new SendMessage(chatId, "Меню2")
+//                        .replyMarkup(
+//                                new InlineKeyboardMarkup(
+//                                new InlineKeyboardButton[][]{
+//                                        {       new InlineKeyboardButton("url").url("www.google.com"),
+//                                                new InlineKeyboardButton("callback_data").callbackData("callback_data111"),
+//                                                new InlineKeyboardButton("Switch!").switchInlineQuery("switch_inline_query")},
+//                                        {       new InlineKeyboardButton("url1").url("www.google.com"),
+//                                                new InlineKeyboardButton("callback_data").callbackData("callback_data222"),
+//                                                new InlineKeyboardButton("Switch!1").switchInlineQuery("switch_inline_query11")},
+//                                           }));
+
+                StringBuilder stringBuilder = new StringBuilder("callback ok");
+                InlineKeyboardMarkup inlineKeybMark = new InlineKeyboardMarkup();
+                inlineKeybMark.addRow (new InlineKeyboardButton[]{
+//                        new InlineKeyboardButton("inline game").callbackGame("pengrad test game description"),
+                        new InlineKeyboardButton("inline ok").callbackData(stringBuilder.toString()),
+                        new InlineKeyboardButton("cancel").switchInlineQuery("callback cancel"),
+                        new InlineKeyboardButton("url").url("www.google.com"),
+                        new InlineKeyboardButton("switch inline").switchInlineQuery("query"),
+                        new InlineKeyboardButton("switch inline current").switchInlineQueryCurrentChat("query")
+                });
+                SendMessage response = new SendMessage(chatId, "Меню3")
+                        .replyMarkup(inlineKeybMark);
+
+//                var next = InlineKeyboardButton.builder()
+//                        .text("Next").callbackData("next")
+//                        .build();
+
+                bot.execute(response);
+
+                SendMessage response2 = new SendMessage(chatId, "Меню2")
+                        .replyMarkup(
+                                new InlineKeyboardMarkup(
+                                        new  InlineKeyboardButton("callback_data").callbackData("callback_data111")
+                                )
+                        );
+                System.out.println(" Распечатываем response --> " + response.toString());
+                bot.execute(response2);
+//                bot.execute(new SendMessage(chatId,  " Это сообщение о нажатии кнопки callback_data ->   " + iKeyboardButtonnew.text()));
+                bot.execute(new SendMessage(chatId, "Вы находитесь в разделе Поддержка "));
+//                continue;
+            } else  {
+                // 01-01-202220.00 Сделать домашнюю работу
+                var matcher = PATTERN.matcher(text);
+                if (matcher.matches()) {
+                    LocalDateTime dateTime = parseTime(matcher.group(1));
+                    if (dateTime == null) {
+                        bot.execute(new SendMessage(chatId, "Формат даты указан неверно!"));
+                        continue;
+                    }
+                    var taskText = matcher.group(3);
+                    NotificationTask task = new NotificationTask();
+                    task.setChatId(chatId);
+                    task.setText(taskText);
+                    task.setDateTime(dateTime);
+                    NotificationTask saved = repository.save(task);
+                    sendMessage(chatId, "Задача запланирована!");
+                    logger.info("Notification task saved: {}", saved);
                 }
-            } else {
-                // Пользователь обращается в первый раз
-                userAlreadyInteracted.put(chatId, true);
-                sendMessage(chatId, "Привет! Это ваше первое обращение. Напишите /menu для доступа к меню.");
+                else {
+                    sendMessage(chatId, "ВЫ не в правильном формате задаете задание !\n"
+                            + "27.12.2023 03:34 Это Верный Формат!");
+                }
             }
         }
         return UpdatesListener.CONFIRMED_UPDATES_ALL;
     }
+//    public static SendMessage sendInlineKeyBoardMessage(long chatId) {
+//        InlineKeyboardMarkup inlineKeyboardMarkup = new InlineKeyboardMarkup();
+//        InlineKeyboardButton inlineKeyboardButton1 = new InlineKeyboardButton();
+//        InlineKeyboardButton inlineKeyboardButton2 = new InlineKeyboardButton();
+//        inlineKeyboardButton1.setText("Тык");
+//        inlineKeyboardButton1.setCallbackData("Button \"Тык\" has been pressed");
+//        inlineKeyboardButton2.setText("Тык2");
+//        inlineKeyboardButton2.setCallbackData("Button \"Тык2\" has been pressed");
+//        List<InlineKeyboardButton> keyboardButtonsRow1 = new ArrayList<>();
+//        List<InlineKeyboardButton> keyboardButtonsRow2 = new ArrayList<>();
+//        keyboardButtonsRow1.add(inlineKeyboardButton1);
+//        keyboardButtonsRow1.add(new InlineKeyboardButton().setText("Fi4a").setCallbackData("CallFi4a"));
+//        keyboardButtonsRow2.add(inlineKeyboardButton2);
+//        List<List<InlineKeyboardButton>> rowList = new ArrayList<>();
+//        rowList.add(keyboardButtonsRow1);
+//        rowList.add(keyboardButtonsRow2);
+//        inlineKeyboardMarkup.setKeyboard(rowList);
+//        return new SendMessage().setChatId(chatId).setText("Пример").setReplyMarkup(inlineKeyboardMarkup);
+//    }
+
+    private LocalDateTime parseTime(String text) {
+        try {
+            return LocalDateTime.parse(text, DATE_TIME_PATTERN);
+        } catch (DateTimeParseException e) {
+            logger.error("Cannot parse date and time: {}", text);
+        }
+        return null;
+    }
+
+    // альтернативный пример
     private void sendMessage(long chatId, String text) {
         bot.execute(new SendMessage(chatId, text));
     }
-
-
 }
+
---
+++ Telegram-Bot/src/main/java/TeamDevelopmentTGbot/settings/MessagesConst.java
@@ -0,0 +1,39 @@
+package TeamDevelopmentTGbot.settings;
+
+import static TeamDevelopmentTGbot.settings.EmojiConst.*;
+
+public class MessagesConst {
+    public final static String HELLO = "! " + HI + "\nС помощью этого бота ты можешь погулять " +
+            "по Русскому музею, Эрмитажу, Третьяковской галерее " +
+            "и полюбоваться картинами некоторых художников.\nПриятного путешествия!";
+
+    public final static String DOG_SHELTER = "Хочешь меня отблагодарить? -\n" +
+            "помоги Фонду \"Помощь бездомным собакам\" " + DOG +
+            "\nhttps://donate.priut.ru";
+
+    public final static String CHOOSE = "Чтобы выбрать музей, воспользуйся клавиатурой " + DOWN;
+
+    public final static String ABC = "Выбери заглавную букву фамилии художника " + DOWN;
+
+    public final static String ART = "Чтобы увидеть картины, нажми на фамилию художника или перейди в полную коллекцию на сайте музея " + DOWN;
+    public final static String UNKNOWN = "Неизвестный запрос... " + THINKING;
+
+    public final static String URL_TG =
+            "https://my.tretyakov.ru/app/gallery/?utm_source=saitgtg&utm_medium=referral&utm_campaign=mainmenu&utm_content=collection";
+
+    public final static String URL_RM =
+            "https://rusmuseumvrm.ru/collections/index.php?ysclid=liiihq7x1m601031688";
+
+    public final static String URL_HERM =
+            "https://collections.hermitagemuseum.org/entity/OBJECT?fundcoll=1418549257";
+
+
+
+    public final static String ABOUT = "Этот проект создан для отдыха " +
+            "и расширения кругозора.\nЕсли нужно заняться делами, " +
+            "приглашаем воспользоваться ботом " +
+            "\nhttps://t.me/SimpleForms_bot";
+
+
+    public final static String VISIT = "Количество посещений " + VISIT_COUNT + " ";
+}
